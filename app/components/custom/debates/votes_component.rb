bb4be6ba98ddfac1665ec222615ecd32abce2ff8
karim-s <114252883+karim-semmoud@users.noreply.github.com>
2023-04-18 08:07:51 +0200

    Changes of Vote count for debates and legislation proposals

---
+++ app/components/custom/debates/votes_component.rb
@@ -0,0 +1,7 @@
+class Debates::VotesComponent < ApplicationComponent; end
+
+require_dependency Rails.root.join("app", "components", "debates", "votes_component").to_s
+
+class Debates::VotesComponent
+  # Your custom logic here
+end
---
+++ app/components/custom/legislation/proposals/votes_component.rb
@@ -0,0 +1,7 @@
+class Legislation::Proposals::VotesComponent < ApplicationComponent; end
+
+require_dependency Rails.root.join("app", "components", "legislation", "proposals", "votes_component").to_s
+
+class Legislation::Proposals::VotesComponent
+  # Your custom logic here
+end
