4c720f271f4d80ce928979e82b540cd60cd65b68
belkacemnetoum <114811109+belkacemnetoum@users.noreply.github.com>
2023-05-16 14:59:31 +0100

    add panel component for custom pages

--- db/pages/welcome_level_three_verified.rb
+++ db/pages/welcome_level_three_verified.rb
@@ -1,17 +1,19 @@
 def generate_content(page)
   page.title = I18n.t("welcome.welcome.title")
 
-  page.content = "<p>#{I18n.t("welcome.welcome.user_permission_info")}</p>
-                  <ul>
-                    <li>#{I18n.t("verification.user_permission_debates")}</li>
-                    <li>#{I18n.t("verification.user_permission_proposal")}</li>
-                    <li>#{I18n.t("verification.user_permission_support_proposal")}</li>
-                    <li>#{I18n.t("verification.user_permission_votes")}</li>
-                  </ul>
+  page.content = "<div class='govuk-panel__body'>
+                    <p>#{I18n.t("welcome.welcome.user_permission_info")}</p>
+                    
+                    <p>#{I18n.t("verification.user_permission_debates")}</p>
+                    <p>#{I18n.t("verification.user_permission_proposal")}</p>
+                    <p>#{I18n.t("verification.user_permission_support_proposal")}</p>
+                    <p>#{I18n.t("verification.user_permission_votes")}</p>
+                    
 
-                  <p>#{I18n.t("account.show.verified_account")}</p>
+                    <p>#{I18n.t("account.show.verified_account")}</p>
 
-                  <p><a href='/'>#{I18n.t("welcome.welcome.start_using_consul")}</a></p>"
+                    <p><a href='/' class='govuk-back-link'>#{I18n.t("welcome.welcome.start_using_consul")}</a></p>
+                  </div>"
   page.save!
 end
 
--- db/pages/welcome_not_verified.rb
+++ db/pages/welcome_not_verified.rb
@@ -1,23 +1,25 @@
 def generate_content(page)
   page.title = I18n.t("welcome.welcome.title")
 
-  page.content = "<p>#{I18n.t("welcome.welcome.user_permission_info")}</p>
-                  <ul>
-                    <li>#{I18n.t("verification.user_permission_debates")}</li>
-                    <li>#{I18n.t("verification.user_permission_proposal")}</li>
-                  </ul>
+  page.content = "<div class='govuk-panel__body'>
+                    <p>#{I18n.t("welcome.welcome.user_permission_info")}</p>
+                  
+                      <p>#{I18n.t("verification.user_permission_debates")}</p>
+                      <p>#{I18n.t("verification.user_permission_proposal")}</p>
+              
 
-                  <p>#{I18n.t("welcome.welcome.user_permission_verify")}</p>
-                  <ul>
-                    <li>#{I18n.t("verification.user_permission_support_proposal")}</li>
-                    <li>#{I18n.t("verification.user_permission_votes")}</li>
-                  </ul>
+                    <p>#{I18n.t("welcome.welcome.user_permission_verify")}</p>
+            
+                      <p>#{I18n.t("verification.user_permission_support_proposal")}</p>
+                      <p>#{I18n.t("verification.user_permission_votes")}</p>
+             
 
-                  <a href='/verification' class='button success radius expand'>
-                    #{I18n.t("welcome.welcome.user_permission_verify_my_account")}
-                  </a>
+                    <a href='/verification' class='button success radius expand'>
+                      #{I18n.t("welcome.welcome.user_permission_verify_my_account")}
+                    </a>
 
-                  <p><a href='/'>#{I18n.t("welcome.welcome.go_to_index")}</a></p>"
+                    <p><a href='/' class='govuk-back-link'>#{I18n.t("welcome.welcome.go_to_index")}</a></p>
+                  </div>"
   page.save!
 end
 if SiteCustomization::Page.find_by(slug: "welcome_not_verified").nil?
