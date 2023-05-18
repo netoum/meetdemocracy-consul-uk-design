417da392b6c0565a710b60758119a8b76cf9d791
karim-s <114252883+karim-semmoud@users.noreply.github.com>
2023-05-18 08:16:11 +0200

    Fix wysiwyg allowed tag and welcome 3 pages

--- db/pages/welcome_level_three_verified.rb
+++ db/pages/welcome_level_three_verified.rb
@@ -1,19 +1,23 @@
 def generate_content(page)
   page.title = I18n.t("welcome.welcome.title")
 
-  page.content = "<div class='govuk-panel govuk-panel--confirmation'
-                    <p>#{I18n.t("welcome.welcome.user_permission_info")}</p>
-                    <ul>
-                      <li>#{I18n.t("verification.user_permission_debates")}</li>
-                      <li>#{I18n.t("verification.user_permission_proposal")}</li>
-                      <li>#{I18n.t("verification.user_permission_support_proposal")}</li>
-                      <li>#{I18n.t("verification.user_permission_votes")}</li>
-                    </ul>
-
-                    <p>#{I18n.t("account.show.verified_account")}</p>
-
-                    <p><a href='/' class='govuk-back-link'>#{I18n.t("welcome.welcome.start_using_consul")}</a></p>
-                  </div>"
+  page.content =
+  "<div class='govuk-panel govuk-panel--confirmation'>
+    <h1 class='govuk-panel__title'>
+      #{I18n.t("account.show.verified_account")}
+    </h1>
+    <div class='govuk-panel__body'>
+      <strong>#{I18n.t("welcome.welcome.user_permission_info")}</strong>
+      <ul class='no-bullet'>
+        <li>#{I18n.t("verification.user_permission_debates")}</li>
+        <li>#{I18n.t("verification.user_permission_proposal")}</li>
+        <li>#{I18n.t("verification.user_permission_support_proposal")}</li>
+        <li>#{I18n.t("verification.user_permission_votes")}</li>
+      </ul>
+    <a href='/' class='govuk-button govuk-button--secondary'>#{I18n.t("welcome.welcome.start_using_consul")}</a>
+    </div>
+    </div>"
+  
   page.save!
 end
 
@@ -23,3 +27,4 @@ if SiteCustomization::Page.find_by(slug: "welcome_level_three_verified").nil?
     I18n.with_locale(locale) { generate_content(page) }
   end
 end
+
