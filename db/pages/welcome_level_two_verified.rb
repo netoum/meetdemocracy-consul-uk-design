e033ce9feb912aa722ad86d2b284641fa1bbaccb
belkacemnetoum <114811109+belkacemnetoum@users.noreply.github.com>
2023-05-22 17:01:50 +0100

    fix welcome pages

--- db/pages/welcome_level_two_verified.rb
+++ db/pages/welcome_level_two_verified.rb
@@ -2,24 +2,27 @@ def generate_content(page)
   page.title = I18n.t("welcome.welcome.title")
 
   page.content = "<div class='govuk-panel govuk-panel--confirmation'>
-                    <p>#{I18n.t("welcome.welcome.user_permission_info")}</p>
-                   
-                      <p>#{I18n.t("verification.user_permission_debates")}</p>
-                      <p>#{I18n.t("verification.user_permission_proposal")}</p>
-                      <p>#{I18n.t("verification.user_permission_support_proposal")}</p>
-               
+                  <h1 class='govuk-panel__title'>#{I18n.t("welcome.welcome.user_permission_info")}</h1>
+                  <div class='govuk-panel__body'>
+                    <ul class='no-bullet'>
+                      <li>#{I18n.t("verification.user_permission_debates")}</li>
+                      <li>#{I18n.t("verification.user_permission_proposal")}</li>
+                      <li>#{I18n.t("verification.user_permission_support_proposal")}</li>
+                    </ul>
+                  </div>
+                  
+                  <h1 class='govuk-panel__title'>#{I18n.t("welcome.welcome.user_permission_verify")}</h1>
+                  <div class='govuk-panel__body'>
+                    <ul class='no-bullet'>
+                      <li>#{I18n.t("verification.user_permission_votes")}</li> 
+                    </ul>
+                  </div>
+                 
+                  <a href='/verification' class='govuk-button govuk-button--secondary'>
+                    #{I18n.t("welcome.welcome.user_permission_verify_my_account")}
+                  </a>
 
-                    <p>#{I18n.t("welcome.welcome.user_permission_verify")}</p>
-                
-                      <p>#{I18n.t("verification.user_permission_votes")}</p>
-               
-
-                    <a href='/verification' class='govuk-back-link'>
-                      #{I18n.t("welcome.welcome.user_permission_verify_my_account")}
-                    </a>
-
-                    <p><a href='/'>#{I18n.t("welcome.welcome.go_to_index")}</a></p>
-                  </div>"
+                  <p><a href='/' class='govuk-back-link'>#{I18n.t("welcome.welcome.go_to_index")}</a></p>"
   page.save!
 end
 if SiteCustomization::Page.find_by(slug: "welcome_level_two_verified").nil?
